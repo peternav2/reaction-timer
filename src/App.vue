b17da95332d1ac6da4c8c11b192e2de5708207e4
Peter Navarrete <peterabcs@gmail.com>
2022-10-23 15:29:33 -0400

    created a basic reaction timer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="handlePlayButton" :disabled="isPlaying"> Play! </button>
+ </div>
+ <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Block
+  },
+  methods: {
+    handlePlayButton() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 3000
+      console.log(this.delay)
+    }
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+
+    }
+  } 
 }
 </script>
 
@@ -20,7 +37,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        click me
+        <p v-if="clicked"> {{ reactionTime }}</p>
+    </div>
+
+</template>
+
+
+
+<script>
+export default {
+    props: ['delay'],
+    // setTimeout(() => {}, delay) is a built-in JavaScript function that takes a function and a delay in milliseconds as arguments. It will execute the function after the delay has passed
+    mounted() {
+        console.log('mounted')
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay)
+    },
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0,
+            clicked: false
+        }
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime+=10
+            }, 10)
+        },
+        stopTimer() {
+            console.log('clicked block')
+            clearInterval(this.timer)
+            this.clicked = true;
+        }
+    },
+    updated() {
+        console.log('updated')
+    },
+    unmounted() {
+        console.log('unmounted')
+    }
+}
+
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background-color: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
+
